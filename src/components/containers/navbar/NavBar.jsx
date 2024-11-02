85792fc621fb56d83dc229165d261c7ef1bbdf73
Carlos Salazar <carlossalazar@MacBook-Air-de-Carlos.local>
2024-11-02 13:46:02 +0100

    Ajustados estilos en navbar y backendslide

--- src/components/containers/navbar/NavBar.jsx
+++ src/components/containers/navbar/NavBar.jsx
@@ -1,23 +1,29 @@
 import React from 'react';
 import { useTranslation } from "react-i18next";
-import './navbar.css';
 import { SocialMedia } from '../../ui';
+import logo from '../../../assets/logo.png';
+import './navbar.css';
 
 const NavBar = ({ scrollToSection, currentIndex }) => {
   const [t] = useTranslation("global");
   const sections = [t("navbar.home"), t("navbar.frontend"), t("navbar.backend"), t("navbar.portfolio", t("navbar.contact"))];
   return (
     <div className='navbar'>
-      {sections.map((section, index) => (
-        <button
-          className={`navbar-button ${currentIndex === index ? "active" : ""}`}
-          key={index}
-          onClick={() => scrollToSection(index)}
-        >
-          <span className={`navbar-button-sub ${currentIndex === index ? "active" : ""}`}>{section}</span>
-        </button>
-      ))}
-      <SocialMedia />
+      <div className={`navbar-logo ${currentIndex === 0 ? "disable" : ""}`}>
+        <img src={logo} alt='logo' />
+      </div>
+      <div className='navbar-buttons'>
+        {sections.map((section, index) => (
+          <button
+            className={`navbar-button ${currentIndex === index ? "active" : ""}`}
+            key={index}
+            onClick={() => scrollToSection(index)}
+          >
+            <span className={`navbar-button-sub ${currentIndex === index ? "active" : ""}`}>{section}</span>
+          </button>
+        ))}
+        <SocialMedia />
+      </div>
     </div>
   );
 };
